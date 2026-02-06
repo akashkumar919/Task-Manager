c6442b49c31e993adbc526edfcbd5406b331a315
Akash Kumar <akashkumar975887@gmail.com>
2026-02-06 16:43:55 +0530

    second commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,47 @@
+// import express from "express";
+// import cors from "cors";
+// import taskRoutes from "./routes/taskRoutes.js";
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/tasks", taskRoutes);
+
+// export default app;
+
+
 import express from "express";
 import cors from "cors";
 import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
-app.use(cors());
+
+// Allowed origins
+const allowedOrigins = [
+  "https://taskmanagerrs.netlify.app", // Production frontend
+  "http://localhost:5173",             // Local frontend
+];
+
+// CORS setup
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 app.use(express.json());
 
+// Routes
 app.use("/api/tasks", taskRoutes);
 
 export default app;
